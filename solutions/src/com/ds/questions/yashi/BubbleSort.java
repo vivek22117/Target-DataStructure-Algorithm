0992ce1f7646ef602d182b8c5f944cae561b0448
vivek22117 <vivekmishra@22117>
2020-01-06 20:27:14 +0530

    Adding packages and example

---
+++ solutions/src/com/ds/questions/yashi/BubbleSort.java
@@ -0,0 +1,26 @@
+package com.ds.questions.yashi;
+
+
+//Question
+//Given an integer array of size N, write an algorithm to sort the
+// elements within the array using BubbleSort Algorithm.
+
+//Handle all edge cases and explain the Space and Time complexity using Big (O) notation.
+
+//Example
+//Input : [5, 4, 11, 18, 1, 9]   Output: [1, 4, 5, 9, 11, 18]
+
+public class BubbleSort {
+    private static int[] inputData = {5, 4, 11, 18, 1, 9};
+
+    public static void main(String[] args) {
+        getSortedArray(inputData);
+    }
+
+    private static int[] getSortedArray(int[] inputData) {
+
+
+        return null;
+    }
+
+}
