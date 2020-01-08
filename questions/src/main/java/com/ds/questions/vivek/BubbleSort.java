4372e2397fd30f875c8e825ec1014bc897c4169b
vivek22117 <vivekmishra@22117>
2020-01-08 20:24:11 +0530

    adding bubble sort file

--- questions/src/main/java/com/ds/questions/vivek/BubbleSort.java
+++ questions/src/main/java/com/ds/questions/vivek/BubbleSort.java
@@ -10,17 +10,41 @@ package com.ds.questions.vivek;
 //Example
 //Input : [5, 4, 11, 18, 1, 9]   Output: [1, 4, 5, 9, 11, 18]
 
+import java.util.Arrays;
+
 public class BubbleSort {
-    private static int[] inputData = {5, 4, 11, 18, 1, 9};
+    private static int[] inputData = {15, 4, 11, 8, 1, 9};
 
     public static void main(String[] args) {
-        getSortedArray(inputData);
+        int[] sortedArray = getSortedArray(inputData);
+        System.out.println(Arrays.toString(sortedArray));
     }
 
+    // Runtime Complexity: O(n2) => Space Complexity: O(1)
     private static int[] getSortedArray(int[] inputData) {
+        if (inputData.length == 0) {
+            return new int[0];
+        }
 
+        if (inputData.length == 1) {
+            return inputData;
+        }
 
-        return null;
+        for (int i = 0; i < inputData.length - 1; i++) {
+            for (int j = i + 1; j < inputData.length; j++) {
+                if (inputData[i] > inputData[j]) {
+                    swap(inputData, i, j);
+                }
+            }
+        }
+
+        return inputData;
+    }
+
+    private static void swap(int[] inputData, int i, int j) {
+        int temp = inputData[i];
+        inputData[i] = inputData[j];
+        inputData[j] = temp;
     }
 
 }
