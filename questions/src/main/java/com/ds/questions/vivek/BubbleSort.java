365b81844f58f85b0325735a450c56bfcbe6bb43
vivek22117 <vivekmishra@22117>
2020-01-08 20:20:44 +0530

    Revert "adding files for bubble sort"
    This reverts commit 3669b4a3799e991426dafd4103b386241ff036ff.

--- questions/src/main/java/com/ds/questions/vivek/BubbleSort.java
+++ questions/src/main/java/com/ds/questions/vivek/BubbleSort.java
@@ -10,41 +10,17 @@ package com.ds.questions.vivek;
 //Example
 //Input : [5, 4, 11, 18, 1, 9]   Output: [1, 4, 5, 9, 11, 18]
 
-import java.util.Arrays;
-
 public class BubbleSort {
-    private static int[] inputData = {15, 4, 11, 8, 1, 9};
+    private static int[] inputData = {5, 4, 11, 18, 1, 9};
 
     public static void main(String[] args) {
-        int[] sortedArray = getSortedArray(inputData);
-        System.out.println(Arrays.toString(sortedArray));
+        getSortedArray(inputData);
     }
 
-    // Runtime Complexity: O(n2) => Space Complexity: O(1)
     private static int[] getSortedArray(int[] inputData) {
-        if (inputData.length == 0) {
-            return new int[0];
-        }
 
-        if (inputData.length == 1) {
-            return inputData;
-        }
 
-        for (int i = 0; i < inputData.length - 1; i++) {
-            for (int j = i + 1; j < inputData.length; j++) {
-                if (inputData[i] > inputData[j]) {
-                    swap(inputData, i, j);
-                }
-            }
-        }
-
-        return inputData;
-    }
-
-    private static void swap(int[] inputData, int i, int j) {
-        int temp = inputData[i];
-        inputData[i] = inputData[j];
-        inputData[j] = temp;
+        return null;
     }
 
 }
