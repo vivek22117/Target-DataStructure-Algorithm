7dbb294604715bd5e99f92d357dc0bff14c88414
Aniket Kulkarni <aniket16kulkarni@gmail.com>
2020-01-09 21:42:39 +0530

    Problem - Plus One to an Array element

---
+++ questions/src/main/java/com/ds/questions/PlusOne.java
@@ -0,0 +1,31 @@
+/**
+ * Given a non-empty array of digits representing a non-negative integer, plus one to the integer.
+
+   The digits are stored such that the most significant digit is at the head of the list, 
+   and each element in the array contain a single digit.
+
+   You may assume the integer does not contain any leading zero, except the number 0 itself.
+   
+   Input: [1,2,3]
+   Output: [1,2,4]
+   Explanation: 123
+               +  1
+              ------
+                124
+   Input: [1,5,9,9]
+   Output: [1,6,0,0]
+   Explanation: 1599
+               +   1
+              -------
+                1600
+ */
+package com.ds.questions;
+
+public class PlusOne {
+
+    // complete the below function
+    public int[] plusOne(int[] digits) {
+
+    }
+
+}
