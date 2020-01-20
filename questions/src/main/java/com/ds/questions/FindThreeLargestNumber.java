4121e38611728aa874b4c3e54244998ce009028d
vivek22117 <vivekmishra@22117>
2020-01-20 20:02:52 +0530

    Adding new question by Vivek!

---
+++ questions/src/main/java/com/ds/questions/FindThreeLargestNumber.java
@@ -0,0 +1,23 @@
+package com.ds.questions;
+
+import java.util.Arrays;
+
+/*Given an input array of integers with negative value also, int[] inputData = {4, 7, -2. 9, 1}
+ * Find out the three largest numbers from the given array without sorting.
+ *
+ * output[] = {4, 7, 9}
+ * */
+public class FindThreeLargestNumber {
+    private static final int[] inputData = {4, 6, -2, 11, 9, -8, 14};
+
+    public static void main(String[] args) {
+        int[] threeLargestNumber = findThreeLargestNumber(inputData);
+        System.out.println(Arrays.toString(threeLargestNumber));
+    }
+
+    private static int[] findThreeLargestNumber(int[] inputData) {
+
+
+        return null;
+    }
+}
