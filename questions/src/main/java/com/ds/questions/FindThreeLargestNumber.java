f823a2726531bf76d49a291ff3be6802fd17be82
r1snow <yashaswi.sharma@siemens.com>
2020-01-24 22:42:05 +0530

    Addressed review comments.

--- questions/src/main/java/com/ds/questions/FindThreeLargestNumber.java
+++ questions/src/main/java/com/ds/questions/FindThreeLargestNumber.java
@@ -8,7 +8,9 @@ import java.util.Arrays;
  * output[] = {4, 7, 9}
  * */
 public class FindThreeLargestNumber {
-    private static final int[] inputData = {4, 6, -2, 11, 9, -8, 14};
+    private static final int[] inputData = {4, 6, 18, -2, 11, 9,20, -8, 14};
+
+
 
     public static void main(String[] args) {
         int[] threeLargestNumber = findThreeLargestNumber(inputData);
@@ -16,19 +18,21 @@ public class FindThreeLargestNumber {
     }
 
     private static int[] findThreeLargestNumber(int[] inputData) {
+        int out[] = new int[3];
         if (inputData == null || inputData.length == 0) {
-            return new int[]{0, 0};
+            return new int[]{-1, -1};
         } else {
             for (int i = 0; i < 3; i++) {
-                for (int j = i + 1; j <= inputData.length - 1; j++)
+                for (int j = i + 1; j <= inputData.length - 1; j++) {
                     if (inputData[j] > inputData[i]) {
                         int temp = inputData[j];
                         inputData[j] = inputData[i];
                         inputData[i] = temp;
                     }
+                }
+                out[i]=inputData[i];
             }
         }
-        return inputData;
-
+        return out;
     }
 }
