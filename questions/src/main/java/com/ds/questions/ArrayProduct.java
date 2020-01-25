0e5c8fd96fac35184f8d31c1abbd8399c9a0b238
r1snow <yashaswi.sharma@siemens.com>
2020-01-25 20:57:05 +0530

    Array Product.

--- questions/src/main/java/com/ds/questions/ArrayProduct.java
+++ questions/src/main/java/com/ds/questions/ArrayProduct.java
@@ -1,13 +1,13 @@
 /**
- * Given an array of integers, return a new array such that 
- * each element at index i of the new array is the product 
+ * Given an array of integers, return a new array such that
+ * each element at index i of the new array is the product
  * of all the numbers in the original array except the one at i.
-
-   For example, if our input was [1, 2, 3, 4, 5], 
-   the expected output would be [120, 60, 40, 30, 24]. 
-   If our input was [3, 2, 1], the expected output would be [2, 3, 6].
-
-   Follow-up: what if you can't use division?
+ * <p>
+ * For example, if our input was [1, 2, 3, 4, 5],
+ * the expected output would be [120, 60, 40, 30, 24].
+ * If our input was [3, 2, 1], the expected output would be [2, 3, 6].
+ * <p>
+ * Follow-up: what if you can't use division?
  */
 package com.ds.questions;
 
@@ -20,12 +20,25 @@ import java.util.Arrays;
 public class ArrayProduct {
 
     public static void main(String[] args) {
-	int[] nums = { 1, 2, 3, 4, 5 };
-	System.out.println(Arrays.toString(productOfNumbers(nums)));
+//        int[] nums = {1, 2, 3, 4, 5};
+        int[] nums = {3, 2, 1};
+        System.out.println(Arrays.toString(productOfNumbers(nums)));
     }
 
     private static int[] productOfNumbers(int[] nums) {
-	return null;
+        int out[]=new int[nums.length];
+        for(int i=0;i<nums.length;i++){
+            int prod=1;
+            for(int j=0;j<nums.length;j++){
+                if(i==j){
+                    continue;
+                }else{
+                    prod=prod*nums[j];
+                }
+                out[i]=prod;
+            }
+        }
+        return out;
     }
 
 }
