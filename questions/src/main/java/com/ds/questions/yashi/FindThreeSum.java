faf1556e32cd96d95bbea0eb884e51c31feca1cd
r1snow <yashaswi.sharma@siemens.com>
2020-01-31 21:37:04 +0530

    Mistakenly add Code remove that.

--- questions/src/main/java/com/ds/questions/yashi/FindThreeSum.java
+++ questions/src/main/java/com/ds/questions/yashi/FindThreeSum.java
@@ -26,46 +26,13 @@ import java.util.Set;
 public class FindThreeSum {
     public static void main(String[] args) {
         int[] nums = new int[]{-1, 0, 1, 2, -1, -4};
-        List<Set<int[]>> sets = threeSum(nums);
-        System.out.println(sets
-        );
-
+        threeSum(nums);
     }
 
-    public static List<Set<int[]>> threeSum(int[] nums) {
-        Arrays.sort(nums);
-        Set<int[]> list = new HashSet<int[]>();
+    public static List<List<Integer>> threeSum(int[] nums) {
 
 
-        List<Set<int[]>> combine = new ArrayList<Set<int[]>>();
-        //        Set<Set<Integer>> combine = new HashSet<Set<Integer>>();
-        for (int i = 0; i < nums.length - 3; i++) {
-            int firstNum = nums[i];
-            int threeSum[] = new int[3];
-            int leftIndex = i + 1;
-            int rightIndex = nums.length - 1;
-            while (leftIndex < rightIndex) {
-                int sum = firstNum + nums[leftIndex] + nums[rightIndex];
-                if (sum > 0) {
-                    rightIndex--;
-                } else if (sum < 0) {
-                    leftIndex++;
-                } else if (sum == 0) {
-                    threeSum[0] = firstNum;
-                    threeSum[1] = nums[leftIndex];
-                    threeSum[2] = nums[rightIndex];
 
-                    list.add(threeSum);
-                    leftIndex++;
-                    rightIndex--;
-                    threeSum= new int[3];
-                } else {
-                    continue;
-                }
-
-            }
-        }
-        combine.add(list);
-        return combine;
+        return null;
     }
 }
