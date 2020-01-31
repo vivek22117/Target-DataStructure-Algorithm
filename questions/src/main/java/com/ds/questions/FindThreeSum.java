cba6887d002a7691a56790fd71789803e7a82821
r1snow <yashaswi.sharma@siemens.com>
2020-01-31 21:38:39 +0530

    New Questions - Find Three sum pairs.

---
+++ questions/src/main/java/com/ds/questions/FindThreeSum.java
@@ -0,0 +1,38 @@
+package com.ds.questions;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * sum of zero.
+ * <p>
+ * Note:
+ * <p>
+ * The solution set must not contain duplicate triplets.
+ * <p>
+ * Example:
+ * <p>
+ * Given array nums = [-1, 0, 1, 2, -1, -4],
+ * <p>
+ * A solution set is:
+ * [
+ * [-1, 0, 1],
+ * [-1, -1, 2]
+ * ]
+ */
+public class FindThreeSum {
+    public static void main(String[] args) {
+        int[] nums = new int[]{-1, 0, 1, 2, -1, -4};
+         threeSum(nums);
+    }
+
+    public static List<List<Integer>> threeSum(int[] nums) {
+
+
+
+    return null;
+    }
+}
