02b46b44f54c2057e0484653c64f7d03998cebcf
r1snow <yashaswi.sharma@siemens.com>
2020-02-02 17:41:24 +0530

    Find Three Largest O(n) solution.

--- questions/src/main/java/com/ds/questions/yashi/FindThreeLargestNumber.java
+++ questions/src/main/java/com/ds/questions/yashi/FindThreeLargestNumber.java
@@ -8,15 +8,15 @@ import java.util.Arrays;
  * output[] = {4, 7, 9}
  * */
 public class FindThreeLargestNumber {
-    private static final int[] inputData = {4, 6, 18, -2, 11, 9,20, -8, 14};
-
+    private static final int[] inputData = {4, 6, 18,42,99,33, -2, 21, 11, 9, 20, -8, 14};
 
 
     public static void main(String[] args) {
-        int[] threeLargestNumber = findThreeLargestNumber(inputData);
+        int[] threeLargestNumber = findThreeLargestNumber2(inputData);
         System.out.println(Arrays.toString(threeLargestNumber));
     }
 
+    //3 O(n)
     private static int[] findThreeLargestNumber(int[] inputData) {
         int out[] = new int[3];
         if (inputData == null || inputData.length == 0) {
@@ -30,9 +30,39 @@ public class FindThreeLargestNumber {
                         inputData[i] = temp;
                     }
                 }
-                out[i]=inputData[i];
+                out[i] = inputData[i];
             }
         }
+        return out;
+    }
+
+
+    //o(n) complexity
+    private static int[] findThreeLargestNumber2(int[] inputData) {
+        int out[] = new int[3];
+        int first = 0;
+        int second = 0;
+        int third = 0;
+        if (inputData == null || inputData.length == 0) {
+            return new int[]{-1, -1};
+        } else {
+            for (int i = 0; i < inputData.length; i++) {
+                if (inputData[i] > first) {
+                    third = second;
+                    second = first;
+                    first = inputData[i];
+                } else if (inputData[i] > second) {
+                    third = second;
+                    second = inputData[i];
+                } else if(inputData[i]>third){
+                    third = inputData[i];
+                }
+            }
+            out[0]=first;
+            out[1]=second;
+            out[2]=third;
+        }
+//        System.out.println("first " + first + " second " + second + " third " + third);
         return out;
     }
 }
